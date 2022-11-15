f2cbc37a04ad1df799c305b494f6d82158159f81
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-11-15 11:26:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,43 +20,50 @@ intS = setInterval(scrollToEndPage,100);
 
 
 function Complete()
-        {
-            var Elem="Фамилия: " + document.Sel1.Family.value +
-                "\nИмя: " + document.Sel1.Name.value +
-                "\nОтчество: " + document.Sel1.Patronymic.value +
-                "\nГод рождения: " + document.Sel1.year.value +
-                "\nПол: " + document.Sel1.Sex.value +
-                "\nТелефон: " + document.Sel1.Phone.value +
-                "\nГород: " + document.Sel1.city.value;
-            alert(Elem);
-        }
-        function CheckAge(year)
-        {
-            if(year<2020&&year>1900)
-                return year;
-            else
-                return  false;
-        }
+{
+    var Elem="Фамилия: " + document.Sel1.Family.value +
+        "\nИмя: " + document.Sel1.Name.value +
+        "\nОтчество: " + document.Sel1.Patronymic.value +
+        "\nГод рождения: " + document.Sel1.year.value +
+        "\nПол: " + document.Sel1.Sex.checked +
+        "\nТелефон: " + document.Sel1.Phone.value +
+        "\nГород: " + document.Sel1.city.value;
+    alert(Elem);
+}
+function CheckAge(year)
+{
+    if(year<2020&&year>1900)
+        return year;
+    else
+        return  false;
+}
 
 
-        function btnClick()
-        {
-            if(document.Test1.Sex[0].checked){
-                document.Test1.Sex[1].click();
-            }else{
-                document.Test1.Sex[0].click();
-            }
-        }
+function btnClick()
+{
+    if(document.Test1.Sex[0].checked){
+        document.Test1.Sex[1].click();
+    }else{
+        document.Test1.Sex[0].click();
+    }
+}
 
 
 
-        function f(){
-            let a=document.getElementById('s1').value;
-            alert(a); // "msk", если выбрана Москва
-            }
+function f()
+{
+    let a=document.getElementById('s1').value;
+    alert(a); //a- значение value
 
+    let b=document.getElementById('s2').value;
+    alert(b); 
+}
 
-            function check()
+function check()
 { 
   document.getElementById('t1').value= "1";
 }
+
+
+
+
