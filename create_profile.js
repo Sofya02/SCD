108464f53f067c87fa95c8f9f14e872bbb8c0c24
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-11-28 10:36:44 +0300

    Add files via upload
    //Нужно создать еще массив, чтобы мы их могли сохранять
    
       const form = document.getElementById('form1')
    
       const formFileds = form.elements;
    
       const submitBtn = form.querySelector('[type="submit"]');
    
       submitBtn.addEventListener('click',clearStorage);
    
    
    
    
       function clearStorage(){
       localStorage.clear();
       }
    
       function changeHandler()
       {
         if(this.type!=='checkbox'){
           console.log(this.name,this.value);
           localStorage.setItem(this.name,this.value);
         }else{
           console.log(this.name,this.checked);
           localStorage.setItem(this.name,this.checked);
         }
       }
    
       function checkStorage(){
         for(let i=0;i<formFileds.length;i++){
           if(formFileds[i].type!='submit'){
             if(formFileds[i].type=='checkbox'){
               formFileds[i].checked=localStorage.getItem(formFileds[i].name);
             }else{
               formFileds[i].value=localStorage.getItem(formFileds[i].name);
             }
           }
         }
         attachEvents();
       }
    
       function attachEvents(){
         for(let i=0;i<formFileds.length;i++){
           formFileds[i].addEventListener('change',changeHandler);
         }
       }
    
       checkStorage();
    
    
        window.addEventListener('storage',()=>{
         console.log('storage has changed');
        })

--- create_profile.js
+++ create_profile.js
@@ -1,3 +1,4 @@
+/*  1 способ
 let formData={};//Получение данных из input
 
 const form = document.querySelector('form');//получение формы для работы
@@ -21,4 +22,109 @@ if(localStorage.getItem('formData')){
   console.log('storage has changed');
  })
 
+*/
+
+/* 2 способ
+//first check to see how many form records exist, if none set to 0 in storage
+if (!localStorage.getItem('form')) {
+    localStorage.setItem('form', 0);
+}
+else {
+    form = localStorage.getItem('form');
+}
+
+//increment to the next form entry
+form++;
+
+//save the form data to localStorage
+localStorage.setItem('form.' + form + '.firstName', JSON.stringify(form));
+
+//lastly, save the number of forms to localStorage
+localStorage.setItem('form', form);
+*/
+
+/* Рабочий метод */
+/*const isCheckboxOrRadio = type => ['checkbox', 'radio'].includes(type);
+
+const {form} = document.forms;
+
+function getFormValue(event) {
+event.preventDefault();
+
+    const data = {};
+
+    for (let field of form) {
+   const {name} = field;
+
+        if (name) {
+       const {type, checked, value} = field;
+
+            data[name] = isCheckboxOrRadio(type) ? checked : value;
+   }
+}
+
+    localStorage.setItem('data',JSON.stringify(data));
+    console.log(data);
+}
+
+form.addEventListener('submit', getFormValue);
+
+
+window.addEventListener('storage',()=>{
+    console.log('storage has changed');
+   })
+*/
+
+
+   const form = document.getElementById('form1')
+
+   const formFileds = form.elements;
+
+   const submitBtn = form.querySelector('[type="submit"]');
+
+   submitBtn.addEventListener('click',clearStorage);
+
+
+
+
+   function clearStorage(){
+   localStorage.clear();
+   }
+
+   function changeHandler()
+   {
+     if(this.type!=='checkbox'){
+       console.log(this.name,this.value);
+       localStorage.setItem(this.name,this.value);
+     }else{
+       console.log(this.name,this.checked);
+       localStorage.setItem(this.name,this.checked);
+     }
+   }
+
+   function checkStorage(){
+     for(let i=0;i<formFileds.length;i++){
+       if(formFileds[i].type!='submit'){
+         if(formFileds[i].type=='checkbox'){
+           formFileds[i].checked=localStorage.getItem(formFileds[i].name);
+         }else{
+           formFileds[i].value=localStorage.getItem(formFileds[i].name);
+         }
+       }
+     }
+     attachEvents();
+   }
+
+   function attachEvents(){
+     for(let i=0;i<formFileds.length;i++){
+       formFileds[i].addEventListener('change',changeHandler);
+     }
+   }
+
+   checkStorage();
+
+
+    window.addEventListener('storage',()=>{
+     console.log('storage has changed');
+    })
 
