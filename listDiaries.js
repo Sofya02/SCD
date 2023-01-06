8444f2078f5faf44f54d0a9c7d6bca5bd153dd70
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2023-01-06 22:09:50 +0300

    Исправления
    1. Модификация: Сортировка списка пловцов в алфавитном порядке
    
    Пока что удалось привязать таблицу к списку.  Но она не привязана к конкретному элементу

--- listDiaries.js
+++ listDiaries.js
@@ -27,9 +27,7 @@ input.addEventListener("keypress",function(keyPressed){
     icon.classList.add('fas', 'fa-trash-alt');
     spanElement.append(icon);
     ul.appendChild(li).append(spanElement,newTodo);
-    //tableCreate();///добавление таблицы исправить
-    //addTable();
-    
+    tableMaker();
     }
     
 });
@@ -44,7 +42,7 @@ saveBtn.addEventListener('click',function(){
 loadTodo();
 
 
-function tableCreate() {
+/*function tableCreate() {
   const body = document.body,
         tbl = document.createElement('table');
   for (let i = 0; i < 10; i++) {
@@ -55,5 +53,43 @@ function tableCreate() {
   }
   body.appendChild(tbl);
 }
-tableCreate();
+*/
 
+function tableMaker()
+{
+  var student;
+  for (var j = 0; j < 10; j++) {
+      student = {
+          number: "№" + j,
+          fio: "ФИО спортсмена" + j,
+          year: "Год" + j,
+          lastRes: "Старый результата" + j,
+          numC: "Тренировки" + j,
+          bestRes: "Лучший результат" + j,
+          stab: "Стабильность" + j,
+          progress: "Прогресс" + j,
+          chance: "Шанс" + j,
+      };
+      var table = document.getElementById("myTable");
+      var row = table.insertRow(j);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      var cell6 = row.insertCell(5);
+      var cell7 = row.insertCell(6);
+      var cell8 = row.insertCell(7);
+      var cell9 = row.insertCell(8);
+  
+      cell1.innerHTML = student.number,
+      cell2.innerHTML = student.fio,
+      cell3.innerHTML = student.year;
+      cell4.innerHTML = student.lastRes,
+      cell5.innerHTML = student.numC,
+      cell6.innerHTML = student.bestRes;
+      cell7.innerHTML = student.stab,
+      cell8.innerHTML = student.progress,
+      cell9.innerHTML = student.chance;
+  }
+}
