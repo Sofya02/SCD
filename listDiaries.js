acda96784ab2eef2e503ddfc905bc4c671fbca22
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2023-01-08 12:16:14 +0300

    Несколько способов создания журнала

--- listDiaries.js
+++ listDiaries.js
@@ -28,6 +28,39 @@ input.addEventListener("keypress",function(keyPressed){
     icon.classList.add('fas', 'fa-trash-alt');
     spanElement.append(icon);
     ul.appendChild(li).append(spanElement,newTodo);
+    for (let i = 0; i < 2; i++) {
+      const tr = t.insertRow();
+
+      for (let j = 0; j < 11; j++) {
+         const td = tr.insertCell();
+      }
+  }
+    li.appendChild(t);
+
+    // Получение элемента таблицы
+    var tables = document.getElementsByTagName("table");
+    // Цикл по таблицам
+    for (var i = 0; i < tables.length; i++) {
+    // Получаем i-ю таблицу
+    var table = tables[i];
+    // Установить идентификатор динамически
+    table.setAttribute("id", i + 1);
+
+    }
+        
+    //Формирование названий столбцов
+    var x=document.getElementById('1').rows[0].cells;
+    x[0].innerHTML="Данные о спортсмене";
+    x[1].innerHTML="Старый результат";
+    x[2].innerHTML="Тренировка1";
+    x[3].innerHTML="Тренировка2";
+    x[4].innerHTML="Тренировка3";
+    x[5].innerHTML="Тренировка4";
+    x[6].innerHTML="Тренировка5";
+    x[7].innerHTML="Лучший результат";
+    x[8].innerHTML="Стабильность";
+    x[9].innerHTML="Прогресс";
+    x[10].innerHTML="Шанс";
    
 
   
