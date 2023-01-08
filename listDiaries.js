057c6d99934e893d397abaef98d7bf9cdeceff95
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2023-01-08 11:59:10 +0300

    Add files via upload

--- listDiaries.js
+++ listDiaries.js
@@ -4,7 +4,7 @@ var ul = document.querySelector("ul");
 var lists = document.querySelectorAll("li");
 var spans = document.getElementsByTagName("span");
 var saveBtn = document.querySelector(".save");
-//var createTable = document.querySelector(".but_table");
+var createTable = document.querySelector(".editTable");
 
 //функция для загрузки todo, если список найден в локальном хранилище.
 function loadTodo(){
@@ -28,10 +28,30 @@ input.addEventListener("keypress",function(keyPressed){
     icon.classList.add('fas', 'fa-trash-alt');
     spanElement.append(icon);
     ul.appendChild(li).append(spanElement,newTodo);
-    for (let i = 0; i < 10; i++) {
+   
+
+  
+   
+}
+    
+});
+
+
+//сохраняем состояние todolist, чтобы пользователь мог получить к нему доступ позже
+saveBtn.addEventListener('click',function(){
+  localStorage.setItem('todoList',ul.innerHTML );
+});
+
+//загружаем список
+loadTodo();
+
+
+
+
+   /* for (let i = 0; i < 2; i++) {
         const tr = t.insertRow();
  
-        for (let j = 0; j < 10; j++) {
+        for (let j = 0; j < 11; j++) {
            const td = tr.insertCell();
         }
     }
@@ -62,20 +82,44 @@ input.addEventListener("keypress",function(keyPressed){
     x[9].innerHTML="Прогресс";
     x[10].innerHTML="Шанс";
 
-    //активация ячейки
-    var a=document.getElementById('1').rows[2].cells;
-    for (var i = 0; i < a.length; i++) {
-        x[0].innerHTML="Время";
+    var select = document.createElement('select');
+    //select.setAttribute("id", sel);
+    select.className="my-select";
+    const selectData = JSON.parse(localStorage.getItem('localData'));
+    // Создадим пустую строку, куда будем закидывать теги option для списка
+    let selectHTML = ''; 
+
+    // Циклом обходим объект, полученный из LS и записываем в selectHTML 
+    for (const key in selectData) {
+      selectHTML += `<option value="${key}">${selectData[key]}</option>`;
     }
-}
-    
-});
 
-//сохраняем состояние todolist, чтобы пользователь мог получить к нему доступ позже
-saveBtn.addEventListener('click',function(){
-  localStorage.setItem('todoList',ul.innerHTML );
-});
+    // Выбираем наш выпадающий список и записываем в него обработанные ранее данные
+    document.querySelector('.my-select').innerHTML = selectHTML;
 
-//загружаем список
-loadTodo();
 
+
+
+    //var tds = document.querySelectorAll('td');
+    var y=document.getElementById('1').rows[1].cells;
+    y[0].append(select);
+    var input2 = document.createElement('input');
+    input2.className = "time";
+  
+
+    y[1] = addEventListener('click', function func(){
+      
+      input2.value = y[1].innerHTML;
+      //y[1].innerHTML="";
+      y[1].appendChild(input2);
+
+      var td=y[1];
+      input2.addEventListener('blur',function(){
+
+        td.innerHTML=this.value;
+        td.addEventListener('click',func);
+
+      });
+
+     // this.removeEventListner('click',func);
+    });*/
